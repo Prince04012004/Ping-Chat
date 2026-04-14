240fce0b455230e7aa7387731d5fe4000a8d9def
Prince04012004 <princekudiya2004@gmail.com>
2026-04-14 23:32:33 +0530

    Fix firebaseconfig

--- Frontend_chat/vite-project/src/firebaseConfig.js
+++ Frontend_chat/vite-project/src/firebaseConfig.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth"; // 1. Ye import zaroori hai
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBnSvLMBtUyzLxAeQB6xjGBLjTAhokEziQ",
   authDomain: "ping-chat-74fdf.firebaseapp.com",
@@ -18,4 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// 2. Auth ko initialize karo aur EXPORT karo
+export const auth = getAuth(app);
