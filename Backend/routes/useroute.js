3a58e31cf0b2ef09c56e1f8b1dc6568374e94876
Prince04012004 <princekudiya2004@gmail.com>
2026-04-18 16:35:37 +0530

    Update backend route

--- Backend/routes/useroute.js
+++ Backend/routes/useroute.js
@@ -6,7 +6,7 @@ import { auth } from "../middlewares/authmiddleware.js";
 const router=express.Router();
 router.get("/searchuser",auth,searchuser)
 router.put("/updateprofile", auth, updateprofile);
-router.post("/block", auth, blockuser);
+router.post("/blockuser", auth, blockuser);
 router.get("/getblockedusers",auth,getblockedusers)
 router.post("/unblockuser",auth,unblockuser)
 router.get("/getprofile/:_id",auth,getprofile)
